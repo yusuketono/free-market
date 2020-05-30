5c4dfe5c8b41825b85e138647892663fa78f9a6e
yusuketono <yusuke.tono@di-v.co.jp>
2020-05-30 14:54:44 +0900

    user.rbに簡易的なバリデーションを記述しよう

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :birthday, :first_name, :last_name, :first_name_reading, :last_name_reading, presence: true
 end
