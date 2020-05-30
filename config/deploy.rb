cf5b9e118267f44fd2923e2dd40127efe1012415
yusuketono <yusuke.tono@di-v.co.jp>
2020-05-30 13:30:20 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.12.1'
 set :application, 'free-market'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:usagrammer/free-market.git'
+set :repo_url,  'github.com:yusuketono/free-market.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5で�
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/free-market_linux2.pem']
+                  keys: ['~/.ssh/free-market.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.150.52', user: 'ec2-user', roles: %w{app db web}
+server '3.113.255.170', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
