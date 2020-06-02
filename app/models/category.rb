5d081d3c6c45d5db66f220c377f2f6257fb270e2
yusuketono <yusuke.tono@di-v.co.jp>
2020-06-02 12:20:54 +0900

    itemモデルとuserモデル,categoryモデルのアソシエーションを設定しよう

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
+  has_many :items
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: :true
+
   validates :name, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  has_many :selling_items, class_name: "Item", foreign_key: "seller_id"
+  has_many :bought_items, class_name: "Item", foreign_key: "buyer_id"
+
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
