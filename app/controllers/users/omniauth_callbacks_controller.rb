709ed10aba0a17a72b7a8973c9b6f2ed5328a876
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-06 15:23:15 +0900

    「redirect_toではなくrenderにする」で登録できるようにしよう

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,14 +3,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_for(:google)
   end
 
-  def callback_for(provider)  
+  def callback_for(provider)
     email = request.env["omniauth.auth"].info.email
     nickname = request.env["omniauth.auth"].info.name
+    ## ーーーーー追加ここからーーーーー
     uid = request.env["omniauth.auth"].uid
     provider = request.env["omniauth.auth"].provider  
     
     SnsCredential.create(uid: uid, provider: provider)
+    ## ーーーーー追加ここまでーーーーー
+
     @user = User.new(email: email, nickname: nickname)
     render layout: 'no_menu', template: 'devise/registrations/new'
+  end
+
+  def failure
+    redirect_to root_path, alert: "エラーが発生しました" and return
   end
 end
