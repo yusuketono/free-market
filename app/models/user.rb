7c4c277b4a976652dccdc04e1beb350e789958a7
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-15 08:11:00 +0900

    itemモデルとprefectureモデルのアソシエーションを設定しよう

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_one :address, dependent: :destroy 
   has_many :selling_items, class_name: "Item", foreign_key: "seller_id"
   has_many :bought_items, class_name: "Item", foreign_key: "buyer_id"
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
