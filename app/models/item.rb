39db0726a74bfb3482d03b3a6b78e6d4d7e9d520
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-24 08:40:39 +0900

    画像の編集と削除に必要な記述を追加しよう

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", optional: :true
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true, update_only: true
 
   enum condition:{
     "新品、未使用": 0,
