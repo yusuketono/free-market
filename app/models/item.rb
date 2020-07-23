8d15add68d862fc917618784fd38b0c162c4f07b
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-24 08:35:04 +0900

    画像の枚数に制限をかけよう

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :images, length: { minimum: 1, maximum: 5, message: "の数が不正です" } 
 
   belongs_to :category
   belongs_to :seller, class_name: "User"
