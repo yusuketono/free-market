aa7185b9f35553a2dc45426223abc0c1d9f8b9b0
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-24 21:41:31 +0900

    本番環境の画像はS3、開発環境ではpublic/uploadsにしよう

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [800, 800]
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  if Rails.env.production?
+    ## 本番環境の時
+    storage :fog
+  else
+    ## 開発環境の時
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
