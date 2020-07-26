29d8908d1494a4a51720f1f5afe8224725447861
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-26 09:38:15 +0900

    灰色の箱をクリックするとfile_fieldが起動するようにしよう

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,3 +1,9 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
+
+  $("#select-image-button").on("click", function () {
+    const file_field = $("#item_images_attributes_0_src"); // 新規画像投稿用のfile_fieldを取得する。
+    file_field.trigger("click"); // file_fieldをクリックさせる。
+  });
+  
 });
