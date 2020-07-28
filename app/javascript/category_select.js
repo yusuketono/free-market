7e63707ab47699867a8bf8d4a09b495333cc1c38
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-29 08:54:30 +0900

    これから実装していくjsファイルを用意しよう

---
+++ app/javascript/category_select.js
@@ -0,0 +1,4 @@
+document.addEventListener('turbolinks:load', function () {
+  if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
+  console.log("hoge");
+});
--- app/javascript/jquery.js
+++ app/javascript/jquery.js
@@ -1,1 +1,2 @@
 import "./item_form"
+import "./category_select"
