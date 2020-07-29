16a249fdb47ea525663bc9e6e9ecfc09543c10d6
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-29 09:33:14 +0900

    受け取ったカテゴリを元に子孫カテゴリを取得しよう

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -5,6 +5,14 @@ class Api::CategoriesController < ApplicationController
     puts "受け取ったカテゴリのID"
     puts params[:category_id]
     puts "-----------------"
+    category = Category.find(params[:category_id])
+    @categories = category.children
+
+    puts "-----------------"
+    puts "子孫カテゴリ"
+    puts @categories.pluck(:name)
+    puts "-----------------"
+    
   end
 
 end
