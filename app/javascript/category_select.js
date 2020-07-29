072f72d81e90db64a21930803509b5acc13bdd62
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-29 09:08:06 +0900

    カテゴリが選択されたら発火するイベントを作ろう

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,4 +1,8 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
-  console.log("hoge");
+
+  $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
+    const category_id = $(this).val();
+    console.log("選択されたカテゴリのID:", category_id);
+  });
 });
