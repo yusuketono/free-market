8e400bd0ccc4378d91048f8cdaee9d6ee192e27e
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-29 09:16:27 +0900

    選択したカテゴリのIDをajaxで送信しよう

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,6 +1,10 @@
 class Api::CategoriesController < ApplicationController
 
   def index
+    puts "-----------------"
+    puts "受け取ったカテゴリのID"
+    puts params[:category_id]
+    puts "-----------------"
   end
 
 end
--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -4,5 +4,18 @@ document.addEventListener('turbolinks:load', function () {
   $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
     const category_id = $(this).val();
     console.log("選択されたカテゴリのID:", category_id);
+    $.ajax({
+      url: "/api/categories",
+      type: "GET",
+      data: {
+        category_id: category_id
+      },
+      dataType: 'json',
+    }).done(function (categories) {
+      console.log("success")
+    })
+    .fail(function () {
+      alert('error');
+    })
   });
 });
