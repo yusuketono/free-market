f99abbf4df47d772f7c938642974b0e307479cfb
yusuketono <yusuke.tono@di-v.co.jp>
2020-07-29 09:43:05 +0900

    受け取った子孫カテゴリを使ってフォームを組み立てよう

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,22 +1,44 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
 
+  function buildCategoryForm(categories) { // 子孫カテゴリのフォームを組み立てる
+    let options = "";
+    categories.forEach(function (category) { // カテゴリを一つずつ渡してoptionタグを一つずつ組み立てていく。
+      options += `
+                  <option value="${category.id}">${category.name}</option>
+                 `;
+    });
+
+    const html = `
+                  <select required="required" class="select-category" id="parent-category" name="item[category_id]">
+                    <option value="">---</option>
+                    ${options}
+                  </select>
+                 `;
+    return html;
+  }
+  /////////buildCategoryForm()ここまで/////////
+
   $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
     const category_id = $(this).val();
     console.log("選択されたカテゴリのID:", category_id);
     $.ajax({
-      url: "/api/categories",
-      type: "GET",
-      data: {
-        category_id: category_id
-      },
-      dataType: 'json',
-    }).done(function (categories) {
-      console.log("success")
-      console.table(categories);
-    })
-    .fail(function () {
-      alert('error');
-    })
+        url: "/api/categories",
+        type: "GET",
+        data: {
+          category_id: category_id
+        },
+        dataType: 'json',
+      }).done(function (categories) {
+        console.log("success")
+        console.table(categories);
+
+        const html = buildCategoryForm(categories);
+        console.log(html);
+
+      })
+      .fail(function () {
+        alert('error');
+      })
   });
 });
